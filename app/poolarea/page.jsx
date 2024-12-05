6860309f2d708f5ae026d9acc5b8fe0d55cd2724
DarthMuzammil <muzammil.inamdar98@gmail.com>
2024-12-05 22:56:58 +0530

    feat: added pool page

---
+++ app/poolarea/page.jsx
@@ -0,0 +1,26 @@
+"use client";
+import HeroSection from "@/components/custom/landing/HeroSection";
+import { services } from "@/lib/poolarea";
+import { ServiceCard } from "@/components/custom/poolarea/service-card";
+import PricingPage  from "@/components/custom/poolarea/pricing-page";
+import WellnessCTA  from "@/components/custom/poolarea/wellness-cta";
+export default function PoolArea() {
+  return (
+    <>
+      <HeroSection />
+      <div className="container mx-auto py-12 px-4">
+        <div className="space-y-[100px]">
+          {services.map((service, index) => (
+            <ServiceCard
+              key={service.id}
+              service={service}
+              reverse={index % 2 !== 0}
+            />
+          ))}
+        </div>
+      </div>
+      <PricingPage/>
+      <WellnessCTA/>
+    </>
+  );
+}
