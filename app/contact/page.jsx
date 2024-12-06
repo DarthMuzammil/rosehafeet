20cf668977c55dfa918c38afc6affbdcd92309fb
DarthMuzammil <muzammil.inamdar98@gmail.com>
2024-12-07 03:29:09 +0530

    f

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,27 +1,7 @@
 "use client";
-import HeroSection from "@/components/custom/landing/HeroSection";
-import { services } from "@/lib/poolarea/english";
-import { ServiceCard } from "@/components/custom/poolarea/service-card";
-import PricingPage  from "@/components/custom/poolarea/pricing-page";
-import WellnessCTA  from "@/components/custom/poolarea/wellness-cta";
-import { heroSectionSlides } from "@/lib/wellness/wellness";
+
 export default function Contact() {
   return (
-    <>
-      <HeroSection slides={heroSectionSlides} />
-      <div className="container mx-auto py-12 px-4">
-        <div className="space-y-[100px]">
-          {services.map((service, index) => (
-            <ServiceCard
-              key={service.id}
-              service={service}
-              reverse={index % 2 !== 0}
-            />
-          ))}
-        </div>
-      </div>
-      <PricingPage/>
-      <WellnessCTA/>
-    </>
+    <div>about</div>
   );
 }
