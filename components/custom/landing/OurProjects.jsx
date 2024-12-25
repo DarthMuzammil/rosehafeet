2a3e3900b716993ca8f42e09bbf645e4eb88ce93
DarthMuzammil <muzammil.inamdar98@gmail.com>
2024-12-25 18:29:17 +0530

    fix

--- components/custom/landing/OurProjects.jsx
+++ components/custom/landing/OurProjects.jsx
@@ -7,7 +7,7 @@ export default function OurProjects() {
   return (
     <section className="flex flex-col items-center py-12 md:px-0  xl:px-10 gap-12">
       <SectionHeading className="p-4">Our Attractions</SectionHeading>
-      <div className="w-full  md:grid p-6 md:p-0 gap-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+      <div className="w-11/12 md:grid p-6 md:p-0 gap-12 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {facilities.map((project, index) => (
           <Services
             key={index + project.heading}
