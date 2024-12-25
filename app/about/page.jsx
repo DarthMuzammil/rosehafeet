6f90eb46d42aab4e391aa57d2290f54481e8c19d
DarthMuzammil <muzammil.inamdar98@gmail.com>
2024-12-25 19:10:19 +0530

    fix

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,14 +1,6 @@
 "use client";
-import OurMission from "@/components/custom/about/OurMission";
-import AboutSection from "@/components/custom/landing/AboutSection";
-import { about } from "@/lib/about/about";
-
 export default function About() {
   return (
-    <>
-
-    <OurMission ourmission={about.ourmission} />
-    <AboutSection />
-    </>
+    <div>hi</div>
   );
 }
